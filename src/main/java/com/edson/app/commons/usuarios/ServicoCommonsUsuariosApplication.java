e3fd8e741c7fed60182ff27153d4970c9ed7f835
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-20 10:06:18 -0300

    Start Library

---
+++ src/main/java/com/edson/app/commons/usuarios/ServicoCommonsUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.edson.app.commons.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoCommonsUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoCommonsUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edson/app/commons/usuarios/ServicoCommonsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.app.commons.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoCommonsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
